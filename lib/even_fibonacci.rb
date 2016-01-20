ef32c636ceaa0daa6423a278ae8f88905f874099
jstooley <jstooley823@gmail.com>
2016-01-20 01:00:16 +0000

    nitrous-backup at Wed Jan 20 01:00:16 UTC 2016

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,10 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  num_one= 1
+  num_two = 2
+  for num_two < limit
+    new = num_two + num_one
+    num_one = num_two
+    num_two = new
+  end
+end
